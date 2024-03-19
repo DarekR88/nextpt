f54f27d8b22fe0922e5c18d94ba7c6e19c72542a
Darek Radke <dradmclad@gmail.com>
2024-03-19 13:39:51 -0500

    banner for plans and pricing page

--- src/app/plans&pricing/page.tsx
+++ src/app/plans&pricing/page.tsx
@@ -1,14 +1,38 @@
 import Image from "next/image";
-import boxingImg from "../../public/images/darkboxing.jpg";
-import plankImg from "../../public/images/darkplank.jpg";
-import laptopImg from "../../public/images/virtualpt.jpg";
-import inpersonImg from "../../public/images/personpt.jpg";
-import classImg from "../../public/images/class.jpg";
+import weightImg from "../../../public/images/weights.jpg"
 
 export default function Plans() {
   return (
     <main>
-      <div>Hello world!</div>
+            <div className="relative lg:w-[1200px] lg:h-[444px] overflow-hidden m-auto lg:mb-[120px] h-[400px] mb-[75px]">
+        {/* Background Image */}
+        <div className="absolute inset-0">
+          <Image
+            src={weightImg}
+            alt="Background"
+            className="w-full h-full object-cover opacity-50"
+          />
+          {/* Dark overlay */}
+          <div className="absolute inset-0 bg-black opacity-50"></div>
+        </div>
+
+        {/* Text Content */}
+        <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center">
+          <h2 className="mb-4 text-5xl font-Anton">
+            PLANS{" "}
+            <span className="text-button-primary">AND</span>
+            {" "}RICING
+          </h2>
+          <button
+            type="button"
+            className="rounded border-2 border-neutral-50 px-7 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+            data-twe-ripple-init
+            data-twe-ripple-color="light"
+          >
+            SIGN UP TODAY
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
